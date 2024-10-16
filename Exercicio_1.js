b10f80478065fc185c3a55d9fbe96561cd2fcedb
Lourense <138793950+Louicidio@users.noreply.github.com>
2024-10-15 22:31:28 -0300

    Update Exercicio_1.js

--- Exercicio_1.js
+++ Exercicio_1.js
@@ -1,7 +1,6 @@
 let num1 = prompt("Digite o primeiro número:");
 let num2 = prompt("Digite o segundo número:");
 
-// Convertendo as entradas para números
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
 
